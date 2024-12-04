f5e58a5b7a94ca5a0fcf313ad769bc830853d58b
kohei <koheitech001@gmail.com>
2024-12-05 07:04:40 +0900

    update

--- install_occ-core.sh
+++ install_occ-core.sh
@@ -1,25 +1,20 @@
 
-git clone https://github.com/tpaviot/pythonocc-core.git
+#git clone https://github.com/tpaviot/pythonocc-core.git
 
-pip3 install numpy
+#pip3 installe numpy
 # PYTHONOCC_INSTALL_DIRECTORY=$(which python3)
-PYTHONOCC_INSTALL_DIRECTORY=$(pyenv prefix)
+PYTHONOCC_INSTALL_DIRECTORY=$(pyenv prefix)/bin/python3.11
 
 cd pythonocc-core && \
     mkdir cmake-build && \
     cd cmake-build && \
     echo "include_directories(${NUMPY_INCLUDE_DIR})" >> ../CMakeLists.txt
-    cmake \sudo make install && \
-     cd ../../
-     rm -rf pythonocc-coresudo make install && \
-     cd ../../
-     rm -rf pythonocc-core
-        -DOCCT_INCLUDE_DIR=/opt/occt781/include/opencascade \
-        -DOCCT_LIBRARY_DIR=/opt/occt781/lib \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DPYTHONOCC_INSTALL_DIRECTORY=$(pyenv prefix) \
-        -DNUMPY_INCLUDE_DIR=$(python3 -c "import numpy; print(numpy.get_include())") \
-        ..  && \
+    cmake -DOCCT_INCLUDE_DIR=/opt/occt781/include/opencascade \
+          -DOCCT_LIBRARY_DIR=/opt/occt781/lib \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DPYTHONOCC_INSTALL_DIRECTORY=$(pyenv prefix)/bin/python3.11 \
+          -DNUMPY_INCLUDE_DIR=$(python3 -c "import numpy; print(numpy.get_include())") \
+           ..  && \
     make -j$(nproc)
 
 sudo make install && \
@@ -28,4 +23,4 @@ sudo make install && \
 
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/occt781/lib' >> $HOME/.bashrc
     
-CMD ["bash"]
+# CMD ["bash"]
